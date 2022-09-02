551470922d46d7751c28897aafc991eb97aeb5fe
Dilip Romi <dilip.romi@ssil.com>
2022-09-02 18:28:34 +0530

    login with token

--- Router/Router.js
+++ Router/Router.js
@@ -7,6 +7,6 @@ Router.get('/student/findByid',Student.getfindByid)
 Router.post('/student/register',Student.postRegi)
 Router.patch('/student/update',Student.studentUpdate)
 Router.delete('/student/delete',Student.selectDelete)
-Router.get('/student/login',Student.UserLogin)
+Router.post('/student/login',Student.UserLogin)
 
 module.exports=Router
